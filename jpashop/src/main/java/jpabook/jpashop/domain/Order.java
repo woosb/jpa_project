676481197ea4c6c9e85ce48580e881a39aaafb26
woosb <woosb0115@gmail.com>
2022-01-03 22:43:39 +0900

    add 연관관계편의 메서드 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -28,14 +28,32 @@ public class Order {
     *   order의 결과가 100개 이면 거기에 속한 member_id를 다시 100번 select 를 날려서 n + 1 문제가 발생한다.
     * */
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
+    //orderDate -> order_date
     private LocalDateTime orderDate;// 주문시간
 
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
+
+    //연관관계 편의 메서드
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
